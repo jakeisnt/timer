35f52999d27161d65e1d270f8399fbd1d64a869b
Jacob Chvatal <jake@isnt.online>
2021-11-08 15:29:04 -0500

    get things building, index.html

--- main.rkt
+++ main.rkt
@@ -1,14 +1,4 @@
-#lang racketscript
-
-(require
- racketscript/htdp/image
- racketscript/htdp/universe
- racket/match
- racket/list)
+#lang racketscript/base
 
 
-(define (start)
-  (big-bang 0
-            (on-tick (lambda (a) (println a)))))
-
-(start)
+(displayln "Hello world")
