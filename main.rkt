4b38a409f1d2b1f5bccfb46025c85b8265934cd4
Jacob Chvatal <jake@isnt.online>
2021-11-08 16:19:25 -0500

    can't get assignment to work?

--- main.rkt
+++ main.rkt
@@ -4,11 +4,21 @@
 
 (define COUNT 100)
 
-(define (intervalFn)
-    (displayln COUNT)
-    (set! COUNT (- COUNT 1)))
+;; get ref to inner html of element with provided id
+(define (get-elem id)
+  ($ (#js*.document.getElementById id) 'innerHTML))
+
+;; display count to element on dom!
+(define (display-count tm)
+  ($/:= (get-elem "clock") ($/str tm)))
+
+
+(define (interval-fn)
+  (displayln COUNT)
+  (display-count (number->string COUNT))
+  (set! COUNT (- COUNT 1)))
 
 (define interval
-  (($ 'setInterval) intervalFn 1000))
+  (($ 'setInterval) interval-fn 1000))
 
 interval
