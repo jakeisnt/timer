4cf5662e0ab351aaf65283b93e34ef2580ccbe87
Jacob Chvatal <jake@isnt.online>
2021-11-09 13:48:13 -0500

    style improvements

--- main.rkt
+++ main.rkt
@@ -15,6 +15,12 @@
   (define urlparams ($/new (#js*.URLSearchParams #js.query-string)))
   (#js.urlparams.get param-name))
 
+(define (get-elem-by-id id)
+  (#js.document.getElementById ($/str id)))
+
+(define (set-html! elem body)
+  ($/:= #js.elem.innerHTML body))
+
 #; { String -> Natural }
 ;; Convert a number of minutes (potentially a string) to seconds
 (define (minutes->seconds tm)
@@ -43,7 +49,7 @@
 ;; get the difference between two times in minutes and seconds
 (define (mins-secs-diff goal cur-time)
   (define dist (- goal cur-time))
-  (define minutes (floor (/ dist 60000)))
+  (define minutes (floor (/ dist 60000))) ;; TODO nicer math
   (define seconds (floor (/ (- dist (* 60000 minutes)) 1000)))
   (values minutes seconds))
 
@@ -58,83 +64,102 @@
   (define-values (mins secs) (mins-secs-diff goal curr-time))
   (and (= mins 0) (= secs 0)))
 
+;; escape javascript semantics madness
+(define (sanitize-number n)
+  (string->number (js-string->string n)))
+
 ;; Get the seconds configured or provided for the timer
 (define (get-seconds)
   (define mb-param (get-query-param MINS-PARAM))
   (define ms-param (get-query-param SECS-PARAM))
   (cond
-    [(not (void? mb-param)) (minutes->seconds mb-param)]
-    [(not (void? ms-param)) ms-param]
+    [(and (not (void? mb-param))
+          (not (void? ms-param)))
+     ;; TODO this was exhibiting JS string concat bug
+     (+ (minutes->seconds (sanitize-number mb-param)) (sanitize-number ms-param))]
+    [(not (void? mb-param)) (minutes->seconds
+                             (sanitize-number mb-param))]
+    [(not (void? ms-param))
+     (sanitize-number ms-param)]
     [else DEFAULT-SECS]))
 
-(define (set-timer goal-time cur-time)
+
+(define START-TIME (get-seconds))
+(define TIME-LEFT START-TIME)
+(define TIMER #f)
+
+;; set the timer and the website state
+(define (set-timer! goal-time cur-time)
   (define-values (mins secs) (mins-secs-diff goal-time cur-time))
+  (set! TIME-LEFT (seconds-left goal-time cur-time))
   (set-elem! "minutes" mins)
   (set-elem! "seconds" secs))
 
-(define (pause-timer in)
-  (#js*.clearInterval in))
+;; Get ref to start button
+(define start-button
+  (get-elem-by-id "start-button"))
+
+(define (pause-timer! in)
+  (#js*.clearInterval in)
+  ($/:= #js.start-button.innerHTML "start")
+  (set! TIMER #f))
+
+;; set initial timer
+(define (set-initial-timer!)
+  (set-timer!
+   (get-stop-time START-TIME) (now)))
 
 #; { Natural [Natural] -> Interval }
 ;; start a timer for a given amount of time and set interval
-(define (start-timer tm [timeout 500])
+(define (start-timer tm [timeout 100])
+  ($/:= #js.start-button.innerHTML "pause")
   (define goal-time (get-stop-time tm))
 
   (define (interval-fn)
     (define cur-time (now))
-    (set-timer goal-time cur-time)
+    (set-timer! goal-time cur-time)
     (when (no-diff? goal-time cur-time)
-      (pause-timer interval)))
+      (pause-timer! interval)))
 
   (define interval
     (#js*.setInterval interval-fn timeout))
+  (set! TIMER interval)
   interval)
 
-
-(define TIME-LEFT (get-seconds))
-(define TIMER-RUNNING #f)
-(define TIMER #f)
-
 ;; Set the timer initially
-(set-timer
- (get-stop-time TIME-LEFT) (now))
+(set-initial-timer!)
 
-;; Get ref to start button
-(define start-button
-  (#js.document.getElementById ($/str "start-button")))
-
+;; Toggle the timer
 (define (toggle-timer)
   (cond
+    ;; If the time left is 0, we're restarting the timer
+    [(and (not TIMER) (= TIME-LEFT 0))
+     (start-timer START-TIME)]
+    ;; If we don't have a timer otherwise, we're resuming the timer
+    [(not TIMER) (start-timer TIME-LEFT)]
+    ;; If we have a timer, pause it (kill it)
+    [TIMER (pause-timer! TIMER)]
+    ;; Otherwise, start the timer?
+    [else (start-timer TIME-LEFT)]))
+
+(define (reset-timer!)
+  (when TIMER (pause-timer! TIMER))
+  (set-initial-timer!))
 
 
-    )
-  )
+($/:= #js.start-button.onclick (λ (_) (toggle-timer)))
 
+(define reset-button
+  (get-elem-by-id "reset-button"))
 
-;; when the timer starts:
-;; - get seconds left
-;; - set interval for the timer
-;; - set button to 'pause'
-;; when it stops:
-;; - set seconds left
-;; - stop interval for the timer
-;; - set button to 'start'
-
-($/:= #js.start-button.onclick
-      (λ (_)
-        (if TIME-LEFT
-            (pause-timer TIMER)
-            (begin
-              (set! TIMER (start-timer (get-seconds)))
-              ($/:= #js.start-button.innerHTML "stop")
-              )
-            )))
-
+($/:= #js.reset-button.onclick
+      (λ (_) (reset-timer!)))
 
 
 ;; TODO
-;; - pause and play timer
-;; - don't start timer on page load
-;; - set timer with ui as well as through url
-;; - enable mins:secs query parameter for time configuration
+;; - Reset button
+;;   - Stop timer
+;;   - Reset to original starting value
+;; - set timer with ui
+;; - add keyboard shortcuts
 ;; - improve UI!
