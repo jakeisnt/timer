ad92dbf8cd5c6e7fa10e019ef3a40023106e2c8c
Jacob Chvatal <jake@isnt.online>
2021-11-09 08:25:04 -0500

    setting innerhtml works but at what cost

--- main.rkt
+++ main.rkt
@@ -1,24 +1,29 @@
 #lang racketscript/base
 
-(require racketscript/interop)
+(require
+ racketscript/browser ; TODO use these bindings!
+ racketscript/interop)
 
 (define COUNT 100)
 
 ;; get ref to inner html of element with provided id
-(define (get-elem id)
-  ($ (#js*.document.getElementById id) 'innerHTML))
+;; (define (get-elem id)
+;;   ($ (#js*.document.getElementById id) 'innerHTML))
 
-;; display count to element on dom!
-(define (display-count tm)
-  ($/:= (get-elem "clock") "asdf"))
+(define (set-elem! id val)
+  (define el (#js.document.getElementById ($/str id)))
+  (println el)
+  ($/:= #js.el.innerHTML ($/str val)))
 
 
 (define (interval-fn)
   (displayln COUNT)
-  (display-count (number->string COUNT))
+  (set-elem! "root" COUNT)
   (set! COUNT (- COUNT 1)))
 
 (define interval
   (($ 'setInterval) interval-fn 1000))
+
+(set-elem! "root" "test!!!!!!!!!!!!")
 
 interval
